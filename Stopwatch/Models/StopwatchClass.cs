e35594a20dbe5472129d16b14b820b92c621c4bd
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-26 21:43:27 +0500

    Created new class with stopwatch prop and some methods

---
+++ Stopwatch/Models/StopwatchClass.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Media;
+using Stopwatch.ViewModels;
+
+namespace Stopwatch.Models
+{
+    internal static class StopwatchClass
+    {
+        private static System.Diagnostics.Stopwatch Stopwatch { get; set; }
+        private static ViewModelLocator ViewLocator { get; set; }
+
+        public static void Start()
+        {
+            ViewLocator.MainWindowModel.IsRunning = true;
+
+            Stopwatch?.Start();
+
+            CompositionTarget.Rendering +=
+                (sender, args) => ViewLocator.MainWindowModel.ElapsedTime = Stopwatch.Elapsed;
+        }
+
+        public static void Stop()
+        {
+            ViewLocator.MainWindowModel.IsRunning = false;
+
+            Stopwatch.Stop();
+
+            CompositionTarget.Rendering -=
+                (sender, args) => ViewLocator.MainWindowModel.ElapsedTime = Stopwatch.Elapsed;
+        }
+
+        public static void Reset()
+        {
+            Stopwatch?.Reset();
+        }
+
+        static StopwatchClass()
+        {
+            ViewLocator = new ViewModelLocator();
+            Stopwatch = new System.Diagnostics.Stopwatch();
+        }
+    }
+}
