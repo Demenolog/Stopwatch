59e621cc678799c002c841265ed9bb66da18854c
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-28 15:28:46 +0500

    Created model for a db table

---
+++ Stopwatch/Models/Records/Records.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch.Models.Records
+{
+    public class Records
+    {
+        public int RecordsId { get; set; }
+
+        [Required]
+        public string Time { get; set; }
+        [Required]
+        public string TotalTime { get; set; }
+    }
+}
