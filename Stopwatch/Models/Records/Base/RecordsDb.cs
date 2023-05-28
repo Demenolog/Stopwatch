a36f1b1ebfd4f47d9bf9ce0d3725a6246e32bc96
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-28 15:21:20 +0500

    Inherited DbContext class

--- Stopwatch/Models/Records/Base/RecordsDb.cs
+++ Stopwatch/Models/Records/Base/RecordsDb.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Stopwatch.Models.Records.Base
 {
-    internal class RecordsDb
+    internal class RecordsDb : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Records.db");
+
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
