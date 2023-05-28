95d60b775350c8fd7dc62f3cfb8c176217985281
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-28 13:14:58 +0500

    Created enum for button statuses

---
+++ Stopwatch/Infrastructure/Constans/ButtonStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch.Infrastructure.Constans
+{
+    internal static class ButtonStatus
+    {
+        public enum Status
+        {
+            Started,
+            Stopped,
+            Continued
+        }
+    }
+}
