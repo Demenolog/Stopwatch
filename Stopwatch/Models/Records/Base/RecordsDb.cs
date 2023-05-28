c6b63b0fd2581755c1ed3f449ab470aadc832919
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-28 15:15:39 +0500

    Removed class to another folder

--- Stopwatch/Models/Records/Base/RecordsDb.cs
+++ Stopwatch/Models/Records/Base/RecordsDb.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stopwatch.Models.Records
+namespace Stopwatch.Models.Records.Base
 {
     internal class RecordsDb
     {
