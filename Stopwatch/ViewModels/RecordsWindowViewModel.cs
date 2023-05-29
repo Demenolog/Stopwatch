f0cefe8574dd4340c27693b85cee96eafef90dc1
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-29 21:20:24 +0500

    Added new prop to view model

--- Stopwatch/ViewModels/RecordsWindowViewModel.cs
+++ Stopwatch/ViewModels/RecordsWindowViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stopwatch.ViewModels.Base;
 
 namespace Stopwatch.ViewModels
 {
-    internal class RecordsWindowViewModel
+    internal class RecordsWindowViewModel : ViewModel
     {
+        #region Title : string - title for records window
+
+        private string _title = "Records";
+
+        public string Title
+        {
+            get => _title;
+            set => SetField(ref _title, value);
+        }
+
+        #endregion Title : string - title for records window
     }
 }
