41ba6fbfecd2890521a56e6132e5a63db049c2d7
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-29 21:21:53 +0500

    Created service to manage records window

---
+++ Stopwatch/Services/RecordsWindowService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stopwatch.Views;
+
+namespace Stopwatch.Services
+{
+    internal static class RecordsWindowService
+    {
+        private static RecordsWindow s_recordsWindow = null;
+
+        public static RecordsWindow RecordsWindow
+        {
+            get => s_recordsWindow;
+            set => s_recordsWindow = value;
+        }
+
+        public static void Create()
+        {
+            if (RecordsWindow != null)
+            {
+                return;
+            }
+
+            RecordsWindow = new RecordsWindow();
+            RecordsWindow.Closed += (o, args) => RecordsWindow = null;
+        }
+
+        public static bool Show()
+        {
+            if (RecordsWindow != null)
+            {
+                RecordsWindow.Show();
+                RecordsWindow.Focus();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
