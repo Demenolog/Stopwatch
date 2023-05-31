4af97922d1a69ebea0f5c4bfb3ddbdad9d908e01
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-31 14:56:24 +0500

    Overrided OnClosed method for MainWindow view

--- Stopwatch/Views/Main/MainWindow.xaml.cs
+++ Stopwatch/Views/Main/MainWindow.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Stopwatch
+﻿using System.Windows.Interop;
+using System;
+using Stopwatch.Services;
+
+namespace Stopwatch
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
+        #region Life cycle
+
+        //protected override void OnSourceInitialized(EventArgs e)
+        //{
+          
+        //}
+
+        protected override void OnClosed(EventArgs e)
+        {
+            RecordsWindowService.RecordsWindow.Close();
+        }
+
+        #endregion
     }
 }
